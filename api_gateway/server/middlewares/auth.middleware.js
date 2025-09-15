135a2fadf86ddee5957d2822d998218469c40bde
Kshitij-Satija <kshitijsatija003@gmail.com>
2025-09-15 17:24:02 +0530

    changes

--- api_gateway/server/middlewares/auth.middleware.js
+++ api_gateway/server/middlewares/auth.middleware.js
@@ -1,7 +1,5 @@
 const ensureAuth = (req, res, next) => {
-  console.log("🔑 ensureAuth called");
-  console.log("req.isAuthenticated():", req.isAuthenticated());
-  console.log("req.user:", req.user);
+  
 
   if (req.isAuthenticated()) {
     return next(); 
